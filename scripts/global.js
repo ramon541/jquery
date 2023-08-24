eb0eab739dbfba5731e60902d3221b97ed4e421d
Ramon Dias Monteiro Silva <122894193+ramon541@users.noreply.github.com>
2023-08-23 21:03:51 -0300

    Finalização dos exercícios

--- scripts/global.js
+++ scripts/global.js
@@ -18,7 +18,64 @@ $(".acordeon-cabecalho").click((e) => {
 // -----------------------------------------
 
 // -------------- Exercício 4 --------------
+
+$(".submit").click((e) => {
+  e.preventDefault();
+  const inputName = $("#inputName").val();
+  let errosNome = "";
+
+  $(".erroNome").html("");
+
+  if (inputName.length < 5 || inputName.length > 40) {
+    errosNome += `O nome precisa ter entre 5 e 40 caracteres! <br>`;
+    $(".erroNome").html(errosNome);
+  }
+
+  if (inputName.search(" ") != -1) {
+    errosNome += "O nome não pode conter espaços.";
+    $(".erroNome").html(errosNome);
+  }
+
+  const inputEndereco = $("#inputEndereco").val();
+  let errosEndereco = "";
+
+  const tamValorEndereco = inputEndereco.split(" ").length;
+  const existeMaisDeUmaPalavra = inputEndereco
+    .split(" ")
+    .every((item) => item != "");
+
+  console.log(existeMaisDeUmaPalavra);
+
+  if (inputEndereco.length < 1) {
+    errosEndereco += `O endereço deve ser preenchido <br>`;
+    $(".erroEndereco").html(errosEndereco);
+  } else if (tamValorEndereco < 2 || !existeMaisDeUmaPalavra) {
+    errosEndereco += `O endereço deve ter no mínimo 2 palavras <br>`;
+    $(".erroEndereco").html(errosEndereco);
+  } else {
+    $(".erroEndereco").html("");
+  }
+});
 // -----------------------------------------
 
 // -------------- Exercício 5 --------------
+let currentIndex = 0;
+const imgs = $(".img");
+const totalImgs = imgs.length;
+$(".img:not(:first)").hide();
+
+function showImage() {
+  imgs.hide();
+  $(`.img:eq(${currentIndex})`).fadeToggle();
+}
+
+$("#prox").click(() => {
+  currentIndex = (currentIndex + 1) % totalImgs;
+  showImage();
+});
+
+$("#ant").click(() => {
+  currentIndex = (currentIndex - 1 + totalImgs) % totalImgs;
+  showImage();
+});
 // -----------------------------------------
